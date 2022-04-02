914fe877ba08753415fc6d0f3c37e2ac0003c466
s.kurochkin <semenkurochkin@gmail.com>
2022-04-02 18:24:48 +0300

    add slider

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+$('.first-slider').slick({
+    autoplay: true,
+    lazyLoad: 'ondemand',
+    prevArrow: "<span class='prev hover-y'></span>",
+    nextArrow: "<span class='next hover-y'></span>",
+});
