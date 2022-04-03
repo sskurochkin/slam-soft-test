f943147e4aafe34c540f9a5c7ddbc8fac100c836
s.kurochkin <semenkurochkin@gmail.com>
2022-04-03 23:02:05 +0300

    50%

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,13 @@ $('.first-slider').slick({
     prevArrow: "<span class='prev hover-y'></span>",
     nextArrow: "<span class='next hover-y'></span>",
 });
+$('.promo__slider').slick({
+    infinite: true,
+    variableWidth: true,
+    centerPadding: '40px',
+    dots: true,
+    slidesToShow: 4,
+    lazyLoad: 'ondemand',
+    prevArrow: "<span class='prev hover-y'></span>",
+    nextArrow: "<span class='next hover-y'></span>",
+});
